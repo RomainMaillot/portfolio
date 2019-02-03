008128fefcd85bd2f3c5f4c2bbf34012d26cd49a
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2019-02-03 13:51:32 +0100

    :sparkles: add 2 new projects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './css/style.styl'
 
+import Project from './js/projects'
+
 // Hot reload
 if(module.hot)
 {
@@ -11,10 +13,21 @@ if(module.hot)
     })
 }
 
+// Create dom
+const $projects = document.querySelectorAll('.js-project')
+let index = 0
+
+for(const $project of $projects)
+{
+    index++
+    const project = new Project($project, index)
+}
+
+
+
 // Canvas
 const $canvas = document.querySelector('canvas')
 const context = $canvas.getContext('2d')
-const pos = {}
 
 // Resize
 let windowWidth = $canvas.width
@@ -27,34 +40,26 @@ const resize = () =>
 
     $canvas.width = window.innerWidth
     $canvas.height = window.innerHeight
-
-    context.beginPath()      // Commencer un tracé
-
-    context.moveTo(0, 0)   // Placer le tracé
-    context.lineTo(windowWidth, 0) // Tracer une ligne
-    context.lineTo(windowWidth, windowHeight)  // Tracer autre une ligne
-    context.lineTo(0, windowHeight)
-    context.closePath()      // Tracer une dernière ligne qui ferme la forme (pas obligatoire)
-
-    context.stroke()         // Faire apparaitre les lignes tracées
 }
 
 window.addEventListener('resize',resize)
 resize()
 
+// Cursor
+const cursor = {}
+window.addEventListener(
+    'mousemove',
+    (_e) =>
+    {
+        cursor.x = _e.clientX 
+        cursor.y = _e.clientY 
+    }
+)
+
 const createCirlce = () =>
 {
-    window.addEventListener(
-        'mousemove',
-        (e) =>
-        {
-            pos.x = e.clientX
-            pos.y = e.clientY
-        }
-    )
-
     context.beginPath()
-    context.arc(pos.x, pos.y, 50, 0, Math.PI * 2)
+    context.arc(cursor.x, cursor.y, 50, 0, Math.PI * 2)
     context.fillStyle = 'black'
     context.fill()
 }
---
+++ src/js/projects.js
@@ -0,0 +1,65 @@
+export default class Project
+{
+    constructor(container, index)
+    {
+        this.container = container
+        this.index = index
+
+        this.createElement()
+    }
+
+    createElement()
+    {
+        if(this.container.dataset.position === 'right')
+        {
+            this.createContentDiv()
+            this.createImgDiv()
+        }
+        else
+        {
+            this.createImgDiv()
+            this.createContentDiv()
+        }
+    }
+
+    createImgDiv()
+    {
+        // Create div img
+        this.imgDiv = document.createElement('div')
+        this.imgDiv.classList.add('img')
+        this.container.appendChild(this.imgDiv)
+
+        // Create img inside of the div
+        this.img = this.container.querySelector('img')
+        this.container.removeChild(this.img)
+        this.imgDiv.appendChild(this.img)
+    }
+
+    createContentDiv()
+    {
+        // Create content
+        this.contentDiv = document.createElement('div')
+        this.contentDiv.classList.add('content')
+        this.container.appendChild(this.contentDiv)
+
+        // Create index
+        this.indexText = document.createElement('h3')
+        this.indexText.innerText = `0${this.index}`
+        this.contentDiv.appendChild(this.indexText)
+
+        // Create textdiv
+        this.textDiv = document.createElement('div')
+        this.textDiv.classList.add('text')
+        this.contentDiv.appendChild(this.textDiv)
+
+        // Create title
+        this.title = document.createElement('h2')
+        this.title.innerText = this.container.dataset.title
+        this.textDiv.appendChild(this.title)
+
+        // Create text
+        this.title = document.createElement('p')
+        this.title.innerText = this.container.dataset.text
+        this.textDiv.appendChild(this.title)
+    }
+}
