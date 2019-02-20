96f2affdc56e5577879bb88c58c3a4d543e32e9a
RomainMaillot <romainmaillot.rm@gmail.com>
2019-02-20 20:32:13 +0100

    :bug: Mouse position

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ for(const $project of $projects)
     const project = new Project($project, index)
 }
 
+const $imgs = document.querySelectorAll('.img')
 
 
 // Canvas
@@ -47,27 +48,71 @@ resize()
 
 // Cursor
 const cursor = {}
+const $mouse = document.querySelector('.mouse')
+let mouseIsHover = false
+
 window.addEventListener(
     'mousemove',
     (_e) =>
     {
         cursor.x = _e.clientX 
         cursor.y = _e.clientY 
+
+        $mouse.style.top = `${cursor.y - 25}px`
+        $mouse.style.left = `${cursor.x - 25}px`
+
+        for(const $img of $imgs)
+        {
+            let rect = $img.getBoundingClientRect()
+            if (cursor.x < rect.right && cursor.x > rect.left && cursor.y > rect.top && cursor.y < rect.bottom) 
+            {
+                $mouse.classList.toggle('hover')
+            }
+        }
     }
 )
+
 
 const createCirlce = () =>
 {
     context.beginPath()
     context.arc(cursor.x, cursor.y, 50, 0, Math.PI * 2)
-    context.fillStyle = 'black'
-    context.fill()
+    context.strokeStyle = 'black'
+    context.stroke()
 }
 
 // Animation
 const loop = () =>
 {
     window.requestAnimationFrame(loop) 
-    createCirlce()   
+    createCirlce()
 }
 window.requestAnimationFrame(loop)
+
+// Mouse animation
+const mouseAnimation = ($item) =>
+{
+    $item.addEventListener(
+        'mouseenter',
+        () =>
+        {
+            $mouse.classList.toggle('hover')
+        }
+    )
+    $item.addEventListener(
+        'mouseleave',
+        () =>
+        {
+            $mouse.classList.toggle('hover')
+        }
+    )
+}
+
+// Hover
+const $nav = document.querySelectorAll('.aside li')
+
+
+for(const $item of $nav)
+{
+    mouseAnimation($item)
+}
